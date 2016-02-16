a990cacc870456a7a4f0f2b09600750886fa9da5
abrahamsk <abrahamskatie@gmail.com>
2016-02-16 02:41:02 -0800

    Get num thesholds in exp1  ［(－－)］zzz  ᕙ(⇀‸↼‶)ᕗ

--- src/sklearn_svm/exp1.py
+++ src/sklearn_svm/exp1.py
@@ -196,6 +196,9 @@ def plot_roc():
         false_pos_rate[i], true_pos_rate[i], _ = roc_curve(y_test[:, i], y_score[:, i])
         roc_auc[i] = auc(false_pos_rate[i], true_pos_rate[i])
 
+    print "Thresholds", _
+    print "Num thresholds", len(_)
+
     # Compute macro-average ROC curve and ROC area
     # macro-averaging gives equal weight to the classification of each label
 
