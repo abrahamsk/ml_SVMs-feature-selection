c8addda07bd1221b79a8b5d5bf07ec1e3bb6e749
abrahamsk <abrahamskatie@gmail.com>
2016-02-16 01:32:29 -0800

    Exp3

--- src/sklearn_svm/exp3.py
+++ src/sklearn_svm/exp3.py
@@ -13,29 +13,13 @@ import numpy as np
 from sklearn import svm
 import matplotlib.pyplot as plt
 
-####################################################################
-# Experiment 2:
-# Feature selection with linear SVM
-# – Using final SVM model from Experiment 1:
-#   1) • Obtain weight vector w
-# Select features:
-# – For m=1 to 57
-#   2) • Select the set of m features that have highest |wm|
-#   3) • Train a linear SVM, SVMm , on all the training data,
-#        only using these m features, and using C* from Experiment 1
-#   4) • Test SVMm on the test set to obtain accuracy.
-# 5) – Plot accuracy vs. m
-####################################################################
-
-
-# class sklearn.svm.SVC(C=1.0, kernel='rbf', degree=3, gamma='auto', ***coef0=0.0***,
-# shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None,
-# verbose=False, max_iter=-1, decision_function_shape=None, random_state=None)[source]
-
-# coef_ : array, shape = [n_class-1, n_features]
-# Weights assigned to the features (coefficients in the primal problem).
-# This is only available in the case of a linear kernel.
-# coef_ is a readonly property derived from dual_coef_ and support_vectors_.
+##########################################################################################
+# Experiment 3: Random feature selection
+# Same as Experiment 2, but for each m, select m features at random from the complete set.
+# This is to see if using SVM weights for feature selection has any advantage over random.
+#
+# Exp3 repeats much of exp2's code, with modifications for new feature selection rules.
+##########################################################################################
 
 # 1) Obtain weight vector w from final SVM model from exp 1
 # get absolute values for weights
@@ -45,14 +29,13 @@ weight_abs_values = np.absolute(model._get_coef())
 # using m features subset
 acc_list = []
 # For m = 2 to 57 (need to have at least 2 features for the SVM to work)
-# xrange is non-inclusive on upper bound, go to 58
 for m in xrange(2, 58):
     # 2) Select the set of m features that have highest |wm|
     # get m largest values for |wm|
 
-    # get indices of m largest weight values
-    indices = heapq.nlargest(m, range(len(weight_abs_values[0])), weight_abs_values[0].take)
-    # get features corresponding to largest weights
+    # select m features at random from the complete set (without replacement)
+    indices = np.random.choice(range(len(weight_abs_values[0])), m, replace=False)
+    # get features corresponding to selected weights
     X_scaled_subset = np.copy(X_scaled[:,[i for i in indices]])
 
     # 3) Train a linear SVM, SVMm , on all the training data, only using
@@ -79,10 +62,10 @@ def plot():
     # label x and y axes, generate title
     plt.xlabel('Number of features')
     plt.ylabel('Accuracy of SVM (Test data)')
-    plt.title('Accuracy vs. m features')
+    plt.title('Accuracy vs. m features, randomly selected')
     plt.legend(loc="lower right")
     plt.show()
 
-# Only plot accuracy if exp2 is the main program
+# Only plot accuracy if exp3 is the main program
 if __name__ == "__main__":
   plot()
